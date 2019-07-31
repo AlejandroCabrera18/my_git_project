8cd75d5cf0fb558573c8b9ee9e21712581b6fb7d
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-30 17:33:30 -0700

    New 2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import webapp2
 import os
 import models
 
-from models import search
+from models import Search
 
 the_jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -23,16 +23,18 @@ class HomePage(webapp2.RequestHandler):
 
 class ResultsPage(webapp2.RequestHandler):
     def post(self):
-        result_template = the_jinja_env.get_template('templates/resultpage.html')
-        age = self.request.get("age")
+        result_template = the_jinja_env.get_template('templates/result.html')
+        age = int(self.request.get("age"))
         mode_of_transportation = self.request.get("mode of travel")
-        range = self.request.get("distance")
-        data = search(age=age,mode_of_transportation=mode_of_transportation,range=range)
-        search.put()
+        range = int(self.request.get("distance"))
+        zip_code = int(self.request.get("zip code"))
+        data = Search(age=age,mode_of_transportation=mode_of_transportation,range=range,zip_code=zip_code)
+        data.put()
         search_data = {
             "age": age,
             "transportation": mode_of_transportation,
-            "range": range
+            "range": range,
+            "location": zip_code
         }
         self.response.write(result_template.render(search_data))
 
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from google.appengine.ext import ndb
 
-class search(ndb.Model):
+class Search(ndb.Model):
     age = ndb.IntegerProperty(required=False)
     mode_of_transportation=ndb.StringProperty(required=True)
     range = ndb.IntegerProperty(required=True)
+    zip_code=ndb.IntegerProperty(required=True)

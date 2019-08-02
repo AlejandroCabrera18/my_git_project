1cb147d3c17264e44b33ae82eb5ab7aab217d3a9
cssi <cssi@cssis-MacBook-Pro.local>
2019-08-02 09:46:17 -0700

    Aug 2 9:46pm

--- main.py
+++ main.py
@@ -25,11 +25,15 @@ class ResultsPage(webapp2.RequestHandler):
         zip_code = self.request.get("zip_code")
         address = self.request.get("address")
         activity = self.request.get("activity")
+        state = self.request.get("state")
+        city = self.request.get("city")
         data = Search(zip_code=zip_code,address=address,activity=activity)
         search_data = {
             "address": address,
             "zip_code":zip_code,
-            "activity": activity
+            "activity": activity,
+            "state": state,
+            "city": city
         }
         self.response.write(result_template.render(search_data))
 
